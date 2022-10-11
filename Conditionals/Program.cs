f09392a52705693b1cd38c8a6aa76ec1ba5a9949
codywashbrook <codywashbrook@hotmail.com>
2022-10-11 08:50:37 -0300

    Initial commit

---
+++ Conditionals/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conditionals");
+            Console.WriteLine();
+
+            int score;
+            string gamertag;
+
+            score = 0;
+            gamertag = "BBL Cody";
+            if (gamertag == "BBL Cody")
+            {
+                score = 1000000000; // cheat
+            }
+            Console.WriteLine("Score: " + score);
+
+            Console.ReadKey(true);
+        }
+    }
+}
